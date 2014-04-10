553778868cb082b16fc9e70d6d307f1b46890acd
O'Koyea Huff-Boone <OKoyea@okoyeas-mbp.domain>
2014-04-09 18:13:33 -0700

    created rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CharityInfinityPrototype::Application.config.secret_key_base = 'dca9efbbdf320658d853c2da86d257460421d1c3e86779effc13ff51985cc0b011d64fa9f8a245fe997b7f48264034bb95a31715d254db59c865aa2759cbfbd4'
