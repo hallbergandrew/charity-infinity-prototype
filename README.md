08bbd7a268b5da2033610ae54369de9b9a27f6ed
O'Koyea Huff-Boone <okoyeahb@gmail.com>
2014-04-14 22:18:24 -0700

    Updated path parameters in controllers and layout

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Charity Infinity
 
 ##ToDo
+* User photo upload authentication error
 * prompt users to fill in profile on first log in 
 * User confirmations, lockouts, and timeouts
 * Admins
--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,11 +1,10 @@
 class DashboardController < ApplicationController
   def show
-    @user = current_user
-    @profile = Profile.find_by(user_id: @user.id)
+    @profile = Profile.find(current_user.id)
     if @profile != nil
       @profile = Profile.find(@profile.user.id)
     else
-      @profile = Profile.create({:user_id => @user.id})
+      @profile = Profile.create({:user_id => current_user.id})
       @profile = Profile.find_by(user_id: @profile.user.id)
     end
   end
