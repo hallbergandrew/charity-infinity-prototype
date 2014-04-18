d0a6b63b498cc74b6d9826a02dcb93c333bfe65e
O'Koyea Huff-Boone <okoyeahb@gmail.com>
2014-04-17 20:43:06 -0700

    fixed photo upload error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
   before_action :authenticate_user!
 
   def after_sign_in_path_for(user)
@@ -10,5 +10,9 @@ class ApplicationController < ActionController::Base
 
   def after_sign_out_path_for(user)
     root_path
+  end
+
+  def handle_unverified_request
+  forgery_protection_strategy.new(self).handle_unverified_request
   end
 end
