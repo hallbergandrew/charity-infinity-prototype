72b6ca05d47826a8318596448027e5bfd1c72303
O'Koyea Huff-Boone <okoyeahb@gmail.com>
2014-04-17 22:02:16 -0700

    refix photo upload error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,8 +11,4 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(user)
     root_path
   end
-
-  def handle_unverified_request
-  forgery_protection_strategy.new(self).handle_unverified_request
-  end
 end
